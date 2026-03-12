a2aac3c5383911062af380c4b73a55130f2b640b
Iskar Deng <denghaowen@126.com>
2026-03-12 10:16:27 -0700

    with clausal comp

--- scripts/test_suite.sh
+++ scripts/test_suite.sh
@@ -2,12 +2,12 @@
 
 python3 /home/dengh/workspace/ANC-learning/scripts/run_matrix_tests.py \
     --ace /home/dengh/workspace/ANC-learning/bin/ace-0.9.34/ace \
-    --grammar /home/dengh/workspace/ANC-learning/grammars/test-korean_20260226_164550/test-korean.dat  \
-    --tests /home/dengh/workspace/ANC-learning/grammars/test-korean_20260226_164550/test_sentences \
+    --grammar /home/dengh/workspace/ANC-learning/grammars/test-korean_20260310_141705/test-korean.dat  \
+    --tests /home/dengh/workspace/ANC-learning/grammars/test-korean_20260310_141705/test_sentences \
     --max-parses 50
 
 python3 scripts/inspect_one.py \
   --ace /home/dengh/workspace/ANC-learning/bin/ace-0.9.34/ace \
-  --grammar /home/dengh/workspace/ANC-learning/grammars/test-korean_20260226_165854/test-korean.dat \
-  --sent "n1-nom n2-gen n3-acc tv1-fin" \
+  --grammar /home/dengh/workspace/ANC-learning/grammars/test-korean_20260310_141705/test-korean.dat \
+  --sent "n1-nom iv1-fin" \
   --mrs
